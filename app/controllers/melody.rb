73e826fc64009441d6bee878639b2eca6bf69c00
lmbd <lmbd.7th@gmail.com>
2015-01-22 01:39:14 +0900

    Fixed

--- app/controllers/melody.rb
+++ app/controllers/melody.rb
@@ -24,19 +24,28 @@ SpreadMelody::App.controllers :melody do
     wav_path = "/data/" + params[:title] + ".wav"
     inner_raw_path = Dir.pwd.to_s + "/public" +  raw_path
     inner_wav_path = Dir.pwd.to_s + "/public" + wav_path
-    unless Melody.find_by(:wav_path => wav_path )
-      open(inner_raw_path, 'wb') do |output|
-        output.write(params[:musicFile][:tempfile].read)
-      end
-      `/usr/local/bin/fluidsynth -F #{inner_wav_path} #{Dir.pwd.to_s}/public/sf/TimGM6mb.sf2 #{inner_raw_path} `
-      m = Melody.new
-      m.author = params[:author]
-      m.name  = params[:title]
-      m.desc = params[:desc]
-      m.raw_path = raw_path
-      m.wav_path = wav_path
-      m.save
+
+    unless params[:musicFile][:type] == "audio/midi" 
+      return "This is not midi file"
     end
-    "succ"
+    if Melody.find_by(:wav_path => wav_path )
+      return  "Duplicated Name"
+    end
+    if params[:musicFile][:tempfile].size > 50000
+     return "Too big File"
+    end 
+
+    open(inner_raw_path, 'wb') do |output|
+      output.write(params[:musicFile][:tempfile].read)
+    end
+      `/usr/local/bin/fluidsynth -F #{inner_wav_path} #{Dir.pwd.to_s}/public/sf/TimGM6mb.sf2 #{inner_raw_path} `
+    m = Melody.new
+    m.author = params[:author]
+    m.name  = params[:title]
+    m.desc = params[:desc]
+    m.raw_path = raw_path
+    m.wav_path = wav_path
+    m.save
+    return "Success"
   end
 end
